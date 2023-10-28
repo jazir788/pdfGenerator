4b7c980821bfb5a313fc0d669defe9bd8823fdb8
jazir788 <jazirsx5@gmail.com>
2023-10-28 12:03:30 +0100

    Added lines to page

--- main.py
+++ main.py
@@ -8,17 +8,25 @@ df = pd.read_csv("topics.csv")
 
 for index,row in df.iterrows():
     pdf.add_page()
+
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,21,200,22)
-    pdf.ln(260)
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
+
+
+    pdf.ln(267)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+
     for i in range(row["Pages"]-1):
         pdf.add_page()
-        pdf.ln(275)
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
+
+        pdf.ln(280)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
